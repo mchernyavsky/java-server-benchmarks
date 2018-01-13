8cf9fa6a1f1e475c096abcbcb04b5d72b964d775
misha <chemike47@gmail.com>
2018-01-13 19:04:22 +0300

    Init submodules

---
+++ client/src/main/java/com/github/mchernyavsky/javaserverbenchmarks/client/ClientMain.java
@@ -0,0 +1,9 @@
+package com.github.mchernyavsky.javaserverbenchmarks.client;
+
+public final class ClientMain {
+    private ClientMain() {
+    }
+
+    public static void main(final String[] args) {
+    }
+}
---
+++ server/src/main/java/com/github/mchernyavsky/javaserverbenchmarks/server/ServerMain.java
@@ -0,0 +1,9 @@
+package com.github.mchernyavsky.javaserverbenchmarks.server;
+
+public final class ServerMain {
+    private ServerMain() {
+    }
+
+    public static void main(final String[] args) {
+    }
+}
